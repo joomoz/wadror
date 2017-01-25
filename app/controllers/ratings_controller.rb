4c705ffe6ae0ba4a787cf5d15bac5cd9fffd4541
Joonas Moilanen <jnsmoilanen@gmail.com>
2017-01-25 21:01:12 +0200

    Added new ratings option

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,22 @@
 class RatingsController < ApplicationController
+
+  # GET /ratings
   def index
-    #render :index    # renderöin näkymätemplate /app/views/ratings/index.html
+    #render :index    # renders viewtemplate /app/views/ratings/index.html
     @ratings = Rating.all
   end
+
+  # GET /ratings/new
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    # Rating.create beer_id: params[:rating][:beer_id], score: params[:rating][:score]
+    # is same as;
+    raise
+    Rating.create params.require(:rating).permit(:score, :beer_id)
+    redirect_to ratings_path
+  end
+
 end
